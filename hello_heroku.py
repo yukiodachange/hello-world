80ca33d4c86f382754ca0c3eb08e3f2c91f92839
小田 裕樹 <yuki_oda@change-jp.local>
2018-09-25 09:55:33 +0900

    local editon

---
+++ hello_heroku.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Herukuｎリクエストを投げる
+"""
+
+import urllib.request
+import json
+
+url = 'https://this-is-heroku.herokuapp.com/hello'
+method = 'POST'
+headers = {'Content-Type' : 'application/json'}
+
+# PythonオブジェクトをJSONに変換する
+obj = {"hello" : "goodmorning"} 
+json_data = json.dumps(obj).encode("utf-8")
+
+req = urllib.request.Request(url, data=json_data, method=method, headers=headers)
+
+try:
+    with urllib.request.urlopen(req) as res:
+        body = res.read().decode('utf-8')
+except urllib.error.HTTPError as err:
+    print(err.code)
+except urllib.error.URLError as err:
+    print(err.reason)
+
+result_objs = json.loads(body.split('\n')[0])
+print (result_objs['response'])
